3f2ba5d349db03b97b15c4f69547c9f4f0f1811f
Cory-Ann Joseph <coryannj@gmail.com>
2024-11-12 18:46:35 +1100

    Q6 cleanup

--- Quest 06.js
+++ Quest 06.js
@@ -6,25 +6,26 @@ const input3 = fs.readFileSync('../quest6_3.txt',{ encoding: 'utf8', flag: 'r' }
 function getPaths(input,partNo){
     let tree = Object.fromEntries(input.split(/[\r\n]+/).map((x)=>x.split(/[:,]/)).map((x)=>[x[0],x.slice(1)]))
     let paths = []
-    let queue = tree['RR'].map((x)=>['RR',x])
+    let queue = tree['RR'].flatMap((x)=> tree[x]!== undefined ? [['RR',x]] : [])
     
     while(queue.length>0){
         let next = queue.shift()  
         let branch = tree[next.at(-1)]
-  
-        if(branch !== undefined){
-            branch.forEach((x)=>{
-                if(x !== '@'){
-                    if(!next.includes(x)){
-                        queue.push(next.concat(x))
-                    }
-                } else {
-                    paths.push(next.concat(x))
+
+        branch.forEach((x)=>{
+            if(x !== '@'){
+                if(!next.includes(x) && tree[x] !== undefined){
+                    queue.push(next.concat(x))
                 }
-            })
-        } 
+            } else {
+                paths.push(next.concat(x))
+            }
+        })        
     }
-    return partNo<2 ? paths.find((x,ix,arr)=>x.includes('@') && arr.findIndex((y,yx)=>yx !== ix && y.includes('@') && y.length === x.length)===-1).join('') : paths.find((x,ix,arr)=>x.includes('@') && arr.findIndex((y,yx)=>yx !== ix && y.includes('@') && y.length === x.length)===-1).map((x)=>x[0]).join('')
+
+    let bestPath = paths.find((x,ix,arr)=> !arr.some((y,yx)=> y.length === x.length && yx !== ix))
+
+    return partNo<2 ? bestPath.join('') : bestPath.map((x)=>x[0]).join('')
 }
 
 console.log(getPaths(input1,1)) // Part 1
