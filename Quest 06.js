27b4d77d004dd081eb7fa1e7ab95d769e8fd9c5b
Cory-Ann Joseph <coryannj@gmail.com>
2024-11-12 19:09:56 +1100

    Q6 cleanup

--- Quest 06.js
+++ Quest 06.js
@@ -4,19 +4,17 @@ const input2 = fs.readFileSync('../quest6_2.txt',{ encoding: 'utf8', flag: 'r' }
 const input3 = fs.readFileSync('../quest6_3.txt',{ encoding: 'utf8', flag: 'r' });
 
 function getPaths(input,partNo){
-    let tree = Object.fromEntries(input.split(/[\r\n]+/).map((x)=>x.split(/[:,]/)).map((x)=>[x[0],x.slice(1)]))
+    let tree = Object.fromEntries(input.split(/[\r\n]+/).map((x)=>x.split(/[:,]/)).map((x,ix,arr)=>[x[0],x.slice(1)]))
     let paths = []
     let queue = tree['RR'].flatMap((x)=> tree[x]!== undefined ? [['RR',x]] : [])
     
     while(queue.length>0){
         let next = queue.shift()  
-        let branch = tree[next.at(-1)]
+        let branch = tree[next.at(-1)].filter((x)=> x === '@' || (!next.includes(x) && tree[x] !== undefined))
 
         branch.forEach((x)=>{
             if(x !== '@'){
-                if(!next.includes(x) && tree[x] !== undefined){
-                    queue.push(next.concat(x))
-                }
+                queue.push(next.concat(x))
             } else {
                 paths.push(next.concat(x))
             }
