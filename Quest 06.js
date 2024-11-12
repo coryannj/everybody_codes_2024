3bf40694669df4b64154f312389b9c355e6ff50d
Cory-Ann Joseph <coryannj@gmail.com>
2024-11-12 18:29:20 +1100

    Q6 cleanup

--- Quest 06.js
+++ Quest 06.js
@@ -3,7 +3,8 @@ const input1 = fs.readFileSync('../quest6_1.txt',{ encoding: 'utf8', flag: 'r' }
 const input2 = fs.readFileSync('../quest6_2.txt',{ encoding: 'utf8', flag: 'r' });
 const input3 = fs.readFileSync('../quest6_3.txt',{ encoding: 'utf8', flag: 'r' });
 
-function getPaths(tree,partNo){
+function getPaths(input,partNo){
+    let tree = Object.fromEntries(input.split(/[\r\n]+/).map((x)=>x.split(/[:,]/)).map((x)=>[x[0],x.slice(1)]))
     let paths = []
     let queue = tree['RR'].map((x)=>['RR',x])
     
@@ -21,21 +22,11 @@ function getPaths(tree,partNo){
                     paths.push(next.concat(x))
                 }
             })
-        } else {
-            paths.push(next)
-        }
+        } 
     }
-    return partNo<2 ? paths.filter((x,ix,arr)=>x.includes('@') && arr.findIndex((y,yx)=>yx !== ix && y.includes('@') && y.length === x.length)===-1)[0].join('') : paths.filter((x,ix,arr)=>x.includes('@') && arr.findIndex((y,yx)=>yx !== ix && y.includes('@') && y.length === x.length)===-1)[0].map((x)=>x[0]).join('')
+    return partNo<2 ? paths.find((x,ix,arr)=>x.includes('@') && arr.findIndex((y,yx)=>yx !== ix && y.includes('@') && y.length === x.length)===-1).join('') : paths.find((x,ix,arr)=>x.includes('@') && arr.findIndex((y,yx)=>yx !== ix && y.includes('@') && y.length === x.length)===-1).map((x)=>x[0]).join('')
 }
 
-// Part 1
-let tree1 = Object.fromEntries(input1.split(/[\r\n]+/).map((x)=>x.split(/[:,]/)).map((x)=>[x[0],x.slice(1)]))
-console.log(getPaths(tree1,1))
-
-// Part 2
-let tree2 = Object.fromEntries(input2.split(/[\r\n]+/).map((x)=>x.split(/[:,]/)).map((x)=>[x[0],x.slice(1)]))
-console.log(getPaths(tree2,2))
-
-// Part 3
-let tree3 = Object.fromEntries(input3.split(/[\r\n]+/).map((x)=>x.split(/[:,]/)).map((x)=>[x[0],x.slice(1)]))
-console.log(getPaths(tree3,3))
+console.log(getPaths(input1,1)) // Part 1
+console.log(getPaths(input2,2)) // Part 2
+console.log(getPaths(input3,3)) // Part 3
