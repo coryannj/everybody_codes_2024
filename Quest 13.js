f62de03275872a140ec06a754577f72f706f8b82
Cory-Ann Joseph <coryannj@gmail.com>
2024-11-22 00:43:46 +1100

    Q13 cleanup

--- Quest 13.js
+++ Quest 13.js
@@ -5,7 +5,7 @@ const input2 = fs.readFileSync('../quest13_2.txt',{ encoding: 'utf8', flag: 'r' 
 const input3 = fs.readFileSync('../quest13_3.txt',{ encoding: 'utf8', flag: 'r' });
 
 function platforms(input,partNo){
-    const lines = input.split(/[\r\n]+/).map((x)=>x.split('').map((y)=>!'# SE'.includes(y)?parseInt(y):y))
+    const lines = input.replaceAll(' ','#').split(/[\r\n]+/).map((x)=>x.split('').map((y)=>!'#SE'.includes(y)?parseInt(y):y))
 
     let start = partNo<3 ? 'S' : 'E';
     let end = partNo<3 ? 'E' : 'S';
@@ -19,35 +19,35 @@ function platforms(input,partNo){
     let min;
     let minObj = {};
     
-    while(queue.findIndex((x)=>x.length>0)!==-1){
+    while(!min){
         let next = queue[queue.findIndex((x)=>x.length>0)].shift();
         let [r,c] = next.at(-1).split('-').map(Number);
     
-        let nextArr = [[r,c+1],[r,c-1],[r+1,c],[r-1,c]].filter(([nr,nc])=>0<=nr && nr<lines.length && 0<=nc && nc<lines[0].length && !'# '.includes(lines[nr][nc]) && !next.includes([nr,nc].join('-')));
-        
-        if(nextArr.length>0){
-            nextArr.forEach(([nr,nc])=>{
-                let nxt = [nr,nc].join('-')
-                let nxtVal = lines[nr][nc] !== end ? lines[nr][nc] : 0
-                let diff = Math.abs(lines[r][c]-nxtVal)
-                let newNext = next.slice()
-                
-                newNext[0]+=(Math.min(diff,10-diff)+1)
-
-                if(minObj[nxt]===undefined||newNext[0]<minObj[nxt]){
-                    minObj[nxt]=newNext[0]
-                    if(lines[nr][nc] !== end){
-                        queue[newNext[0]].push(newNext.concat(nxt))
-                    } else {
-                        min = newNext[0]
-                    }
+        [[r,c+1],[r,c-1],[r+1,c],[r-1,c]].filter(([nr,nc])=> 
+            0 <= nr && nr < lines.length 
+            && 0 <= nc && nc<lines[0].length 
+            && lines[nr][nc] !== '#'
+            && !next.includes([nr,nc].join('-'))
+        ).forEach(([nr,nc])=>{
+            let nxt = [nr,nc].join('-');
+            let nxtVal = lines[nr][nc] !== end ? lines[nr][nc] : 0;
+            let diff = Math.abs(lines[r][c]-nxtVal);
+            let newNext = next.slice();
+            
+            newNext[0]+=(Math.min(diff,10-diff)+1)
+            
+            if(minObj[nxt]===undefined||newNext[0]<minObj[nxt]){
+                minObj[nxt]=newNext[0]
+                if(lines[nr][nc] !== end){
+                    newNext.push(nxt)
+                    queue[newNext[0]].push(newNext)
+                } else {
+                    min = newNext[0]
                 }
-            })
-        }
-       
-        if(min !== undefined)break;
-        
+            }
+        })   
     }
+
     return min
 }
 
